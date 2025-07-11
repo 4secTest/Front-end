7648356a84b8c4ca0e41c48c95f6731d7a060a6a
4secTest <shylorantonia@gmail.com>
2025-07-11 11:49:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+console     = require('console');
+const { app, BrowserWindow } = require('electron'); 
+
+let mainWindow; 
+
+function createWindow () {
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+}
